d4b65d039b006f4372ce18fd07e672d36c310397
Muhammad Saad <itsmsaadhere@gmail.com>
2021-02-13 18:06:47 +0500

    Checking Surge Deployment

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header = () => {
     return (
         <h2>
-            Expense Tracker by Muhammad Saad
+            Expense Tracker by M.Saad
         </h2>
     )
 }
